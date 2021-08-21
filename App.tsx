5515562ed016cb1f8560f886892d68974f984552
AmirMohammad Pirzadeh <am.pirzadeh@gmail.com>
2021-08-21 20:47:30 +0430

    Basics of the template

--- App.tsx
+++ App.tsx
@@ -1,21 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import { useColorScheme } from "react-native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import * as SecureStore from "expo-secure-store";
+import { NavigationContainer } from "@react-navigation/native";
+import { Provider as PaperProvider } from "react-native-paper";
+import { HomeScreen, LoginScreen as LoginScreen } from "./screens";
+import { AuthContext } from "./auth/AuthContext";
+import { CombinedDarkTheme, CombinedDefaultTheme } from "./theme";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
+  const scheme = useColorScheme();
+  const [token, setToken] = useState("");
+  const theme = scheme === "dark" ? CombinedDarkTheme : CombinedDefaultTheme;
+
+  useEffect(() => {
+    SecureStore.getItemAsync("userToken")
+      .then((userToken) => {
+        setToken(userToken || "");
+      })
+      .catch((e) => console.log({ e }));
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider theme={theme}>
+      <NavigationContainer theme={theme}>
+        <StatusBar style="auto" />
+        <AuthContext.Provider value={{ token, setToken }}>
+          <Stack.Navigator>
+            {token ? (
+              <Stack.Screen name="Home" component={HomeScreen} />
+            ) : (
+              <Stack.Screen
+                name="Login"
+                options={{ title: "Login / Register" }}
+                component={LoginScreen}
+              />
+            )}
+          </Stack.Navigator>
+        </AuthContext.Provider>
+      </NavigationContainer>
+    </PaperProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
